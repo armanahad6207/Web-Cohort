60b044b9dfd21d2a54a44dac5c6f85589bd965cd
armanahad6207 <armanahad7549@gmail.com>
2025-03-21 11:22:34 +0530

    check ISBN number

--- DSA/Assignment-1/ISBNnumber.js
+++ DSA/Assignment-1/ISBNnumber.js
@@ -1,13 +1,23 @@
-let n = Number("1234567899");
 
-let count = 10;
-let sum = 0;
-while (n > 0) {
-  sum = sum + (n % 10) * count;
 
-  n = Math.floor(n / 10);
-  count--;
+function checkISBN(num) {
+  if (num.length !== 10) {
+    return "enter valid isbn number ";
+  }
+  let n = Number(num);
+  let count = 10;
+  let sum = 0;
+  while (n > 0) {
+  
+    sum = sum + (n % 10) * count;
+   
+
+
+    n = Math.floor(n / 10);
+    count--;
+  }
+  if (sum % 11 === 0) return "isbn number";
+  else return " not isbn number";
 }
 
-if (sum % 11 === 0) console.log("isbn number");
-else console.log(" not isbn number");
+console.log(checkISBN("0471958697"))
